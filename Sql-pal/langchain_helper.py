92d208d5542a2aa7d6cda9ad8b694e74775a32d2
Sheryar <alisharyar93@gmail.com>
2025-09-08 20:32:31 +0500

    ` A Sql-bot that executes query using natural english language `

--- Sql-pal/langchain_helper.py
+++ Sql-pal/langchain_helper.py
@@ -1,19 +1,41 @@
 import os
-import pymysql
-import google.generativeai as genai
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
+from langchain.chains import create_sql_query_chain
 
 load_dotenv()
 
-def sql_bot():
+
+def sql_bot_with_query():
 
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
-        google_api_key=os.getenv("GOOGLE_API_KEY")
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0.1
     )
+
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB_NAME")
+
+    db = SQLDatabase.from_uri(
+        f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",
+        sample_rows_in_table_info=3
+    )
+    query_chain = create_sql_query_chain(llm, db)
+    return query_chain, db
+
+def sql_bot_direct():
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0.1
+    )
+
     db_user = os.getenv("DB_USER")
     db_password = os.getenv("DB_PASSWORD")
     db_host = os.getenv("DB_HOST")
@@ -24,16 +46,4 @@ def sql_bot():
         sample_rows_in_table_info=3
     )
 
-
-    chain=SQLDatabaseChain.from_llm(
-        llm,
-        db,
-        verbose=True,
-        return_direct=True
-    )
-    return chain
-
-if __name__ == "__main__":
-    chain = sql_bot()
-    result = chain.run("How many t-shirt do we have left for Nike in extra small size and white color?")
-    print("Query Result:", result)
+    return llm, db
